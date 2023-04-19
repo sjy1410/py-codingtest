a48f0f36728ad79e1673262d7670a024e5ca544d
songjuyoung-1410 <chicken010208@gmail.com>
2023-04-19 11:11:14 +0900

    숫자 카드 게임

--- solve.py
+++ solve.py
@@ -1,17 +1,9 @@
-n = input()
-row = int(n[1])  # 행
-column = ord(n[0])-96  # 열
+n, m = map(int, input().split())  # 행 열
 
-# 나이트의 이동방식
-steps = [[1, 2], [1, -2], [2, 1],
-         [2, -1], [-1, 2], [-1, -2], [-2, 1], [-2, -1]]
+re = 0
+for i in range(n):
+    card = list(map(int, input().split()))
+    mic = min(card)
+    re = max(re, mic)
 
-# 이동 가능한 경우의 수 카운트
-cnt = 0
-for i in steps:
-    nx = row+i[0]
-    ny = column+i[1]
-    if nx <= 8 and nx >= 1 and ny >= 1 and ny <= 8:
-        cnt += 1
-
-print(cnt)
+print(re)
