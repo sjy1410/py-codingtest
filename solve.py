8826b9f359594b16710915f2b925c03a0406146c
songjuyoung-1410 <chicken010208@gmail.com>
2023-05-10 09:17:40 +0900

    배열에 찾는값이 없을 때의 예외처리

--- solve.py
+++ solve.py
@@ -1,4 +1,11 @@
-a, b = map(int, input().split())
+n, target = map(int, input().split())
+arr = list(map(int, input().split()))
 
-print("a = %d" % a)
-print("b = %d" % b)
+
+def f(n, target):
+    for i in arr:
+        if i == target:
+            return arr.index(target)+1
+    return '원소가 존재하지 않습니다.'
+
+print(f(n,target))
